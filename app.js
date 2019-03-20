e17c42605abdd6cca3e7253c258cfcd012b405a7
Tia Rose <shortyshort789@aol.com>
2019-03-20 13:50:03 -0700

    have table displayed, just need to append it correctly

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-console.log('Hello World');
-
 //---------------
 // Global Functions
 //----------------
 
 var renderStores = function(cookieStoresEl){
-  var article = document.createElement('article');
+  var section = document.createElement('section');
   // article.setAttribute('id', `$cookieStoreOne.name.toLowerCase().replace( ' S, ')`)
-  cookieStoresEl.appendChild(article);
+  cookieStoresEl.appendChild(section);
 
   //Creating h3 Cookie One and appending it
   var h3 = document.createElement('h3'); //Step 2
   h3.textContent = this.name; // Step 3
-  article.appendChild(h3); //Step 4
+  section.appendChild(h3); //Step 4
 
   //Creating h4 Cookie One Location and appending it
   var h4 = document.createElement('h4');
   h4.textContent = this.location;
-  article.appendChild(h4);
+  section.appendChild(h4);
+
+  //Creating h4 Cookie One Location and appending it
+  var h4 = document.createElement('h4');
+  h4.textContent = ('Min. Hours '+`${this.minHourCust}`);
+  section.appendChild(h4);
+
+  var h4 = document.createElement('h4');
+  h4.textContent = ('Max. Hours '+`${this.maxHourCust}`);
+  section.appendChild(h4);
 
   //Creating ol for li for Cookie Store one
   var ul = document.createElement('ul');
-  article.appendChild(ul);
+  section.appendChild(ul);
 
-  //Creating for loop for li for Cookie Store One
-  //textContent template '6am: 13 cookies
+  //Creates lis for hours and then creates lis for random cookies; displayed like hours:randomCookies
   for(var i = 0; i < hoursOpen.length; i++){
     var li= document.createElement('li');
 
@@ -36,61 +42,47 @@ var renderStores = function(cookieStoresEl){
 
   }
 };
+//--------------------------
+//Constructor Function
+//--------------------------
 
-// function tableCreate (){
-//   var article = document.createElement('article');
-//   var tbody = document.createElement('tbody');
-//   article.appendChild(tbody);
-//   for (var i = 0; i < 15; i++){
-//     var tr = document.createElement('tr');
-//     var td = document.createElement('td');
-//     td.textContent = hoursOpen;
-//     tbody.appendChild(tr);
-//     tr.appendChild(td);
-//   }
-//   // tbody.appendChild(tr);
-//   // tr.appendChild(td);
-//   console.log(tableCreate());
-// }
+function SalmonCookieStores(name, location, minHourCust, maxHourCust, avgCookie){
+  this.name = name;
+  this.location = location;
+  this.minHourCust = minHourCust;
+  this.maxHourCust = maxHourCust;
+  this.avgCookie = avgCookie;
+}
 
-// var cookieStoreTable = function() {
 
-//   var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-//   // get the reference for the body
-//   var body = document.getElementsByTagName('body');
-
-//   // creates a <table> element and a <tbody> element
-//   var tbl = document.createElement('table');
-//   var tblBody = document.createElement('tbody');
-
-//   // creating all cells
-//   for (var i = 0; i < hoursOpen.length; i++) {
-//     // creates a table row
-//     var row = document.createElement('tr');
-
-//     for (var j = 0; j < this.randomCookie; j++) {
-//       // Create a <td> element and a text node, make the text
-//       // node the contents of the <td>, and put the <td> at
-//       // the end of the table row
-//       var cell = document.createElement('td');
-//       var cellText = document.createTextNode('cell in row '+i+', column '+j);
-//       cell.appendChild(cellText);
-//       row.appendChild(cell);
-//     }
-
-//     // add the row to the end of the table body
-//     tblBody.appendChild(row);
-//   }
-
-//   // put the <tbody> in the <table>
-//   tbl.appendChild(tblBody);
-//   // appends <table> into <body>
-//   // body.appendChild(tbl);
-//   // sets the border attribute of tbl to 2;
-//   tbl.setAttribute('border', '2');
+SalmonCookieStores.prototype.randomCustHour = function (){
+  var randomNum = Math.floor(Math.random() * 50);
+  console.log(randomNum);
+  return randomNum;
+};
+SalmonCookieStores.prototype.cookiePur = function(){
+  var result = Math.floor(Math.random() * (this.maxHourCust - this.minHourCust) + this.minHourCust);
+  return result;
+};
+// SalmonCookieStores.prototype.cookieStoreTableHeadCell= function(){
+//   var resultElememt = document.createElement('th');
+//   resultElememt.textContent = this.cookiePur();
+//   return resultElememt;
 // };
+SalmonCookieStores.prototype.render = renderStores;
 
-// 1. parent
+//--------------------------
+// Define all data
+//--------------------------
+
+// var allCookieStores = [];
+var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+//----------
+//TABLE
+//----------
+
+// 1. Create Parent Element
 var cookieStoreTable = document.getElementById('cookieStoreTable');
 // 2. create element
 var theadEl = document.createElement('thead');
@@ -103,26 +95,27 @@ cookieStoreTable.appendChild(tbodyEl);
 var tr = document.createElement('tr');
 theadEl.appendChild(tr);
 
-// var th = document.createElement('th');
-// tr.appendChild(th);
-
-th = document.createElement('th');
-tr.textContent = '6:00am';
-tr.appendChild(th);
 
 var th = document.createElement('th');
 tr.appendChild(th);
 
-th = cookieStoreTableHeadCell('6:00am');
-tr.appendChild(th);
-
 //fucntion cookieStoreTabHeadCell(typecontent, attr);
 
-function cookieStoreTableHeadCell(content) {
-  var resultElememt = document.createElement('th');
-  resultElememt.textContent = content;
-  return resultElememt;
-}
+// //Function creates table header
+// function cookieStoreTableHeadCell(content) {
+//   var resultElememt = document.createElement('th');
+//   resultElememt.textContent = content;
+//   return resultElememt;
+// }
+
+// //Loops through all hours open and creates/displays row
+// for(var i = 0; i < hoursOpen.length; i++){
+//   th = cookieStoreTableHeadCell(hoursOpen[i]);
+//   tr.appendChild(th);
+// }
+
+// th = cookieStoreTableHeadCell('7am');
+// tr.appendChild(th);
 
 var tbody = document.createElement('tbody');
 cookieStoreTable.appendChild(tbody);
@@ -130,88 +123,155 @@ cookieStoreTable.appendChild(tbody);
 tr = document.createElement('tr');
 tbody.appendChild(tr);
 
-th = cookieStoreTableHeadCell('1st and Pike');
-tbody.appendChild(th);
+// th = cookieStoreTableHeadCell('1st and Pike');
+// tbody.appendChild(th);
 
+//CAN DELETE
 var td = document.createElement('td');
-td.textContent = '20 cookies';
+// td.textContent = '20 cookies';
 tr.appendChild(td);
 
-// td = document.createElement('td');
-// td.textContent = '30 cookies';
+// td = cookieStoreTableDataCell('20 cookies');
 // tr.appendChild(td);
 
-td = cookieStoreTableDataCell('20 cookies');
-tr.appendChild(td);
-
+//DATA CELL
 function cookieStoreTableDataCell(content){
   var resultElement = document.createElement('td');
   resultElement.textContent = content;
   return resultElement;
 }
 
+
+//Function creates table header
+function cookieStoreTableHeadCell(content) {
+  var resultElememt = document.createElement('th');
+  resultElememt.textContent = content;
+  return resultElememt;
+}
+
+//Loops through all hours open and creates/displays row
+for(var i = 0; i < hoursOpen.length; i++){
+  th = cookieStoreTableHeadCell(hoursOpen[i]);
+  tr.appendChild(th);
+}
+//CookieStoreOne Data Displayed
+th = cookieStoreTableHeadCell('1st and Pike');
+tbody.appendChild(th);
+
+tr = document.createElement('tr');
+tbody.appendChild(tr);
+
+var storeOneFromConstructorFunction = new SalmonCookieStores('Cookie Store One', '1st and Pike', 23, 65, 6.3);
+for(var i=0; i < hoursOpen.length; i++){
+  td = cookieStoreTableDataCell(storeOneFromConstructorFunction.cookiePur());
+  tr.appendChild(td);
+}
+
+//CookieStoreTwo Data Displayed
+th = cookieStoreTableHeadCell('SeaTac Airport');
+tbody.appendChild(th);
+
+tr = document.createElement('tr');
+tbody.appendChild(tr);
+
+var storeTwoFromConstructorFunction = new SalmonCookieStores('Cookie Store Two', 'SeaTac Airport', 3, 24, 1.2);
+for(var i=0; i < hoursOpen.length; i++){
+  td = cookieStoreTableDataCell(storeTwoFromConstructorFunction.cookiePur());
+  tr.appendChild(td);
+}
+
+//CookieStoreThree Data Displayed
+th = cookieStoreTableHeadCell('Seattle Center');
+tbody.appendChild(th);
+
+tr = document.createElement('tr');
+tbody.appendChild(tr);
+
+var storeThreeFromConstructorFunction = new SalmonCookieStores('Cookie Store Three', 'Seattle Center', 11, 38, 3.7);
+for(var i=0; i < hoursOpen.length; i++){
+  td = cookieStoreTableDataCell(storeThreeFromConstructorFunction.cookiePur());
+  tr.appendChild(td);
+}
+
+//CookieStoreFour Data Displayed
+th = cookieStoreTableHeadCell('Capitol Hill');
+tbody.appendChild(th);
+
+tr = document.createElement('tr');
+tbody.appendChild(tr);
+
+var storeFourFromConstructorFunction = new SalmonCookieStores('Cookie Store Four', 'Capitol Hill', 20, 38, 2.3);
+for(var i=0; i < hoursOpen.length; i++){
+  td = cookieStoreTableDataCell(storeFourFromConstructorFunction.cookiePur());
+  tr.appendChild(td);
+}
+
+//CookieStoreFive Data Displayed
+th = cookieStoreTableHeadCell('Alki');
+tbody.appendChild(th);
+
+tr = document.createElement('tr');
+tbody.appendChild(tr);
+
+var storeFiveFromConstructorFunction = new SalmonCookieStores('Cookie Store Five', 'Alki', 2, 16, 4.6);
+for(var i=0; i < hoursOpen.length; i++){
+  td = cookieStoreTableDataCell(storeFiveFromConstructorFunction.cookiePur());
+  tr.appendChild(td);
+}
+
 //if (attr){
 //   resultElement.setAttribute(attr);
 // }resultElememt.textContent = content;
 
-function SalmonCookieStores(name, location, minHourCust, maxHourCust, avgCookie){
-  this.name = name;
-  this.location = location;
-  this.minHourCust = minHourCust;
-  this.maxHourCust = maxHourCust;
-  this.avgCookie = avgCookie;
-  this.randomCookie = [];
-  allCookieStores.push(this);
-}
 
-
-SalmonCookieStores.prototype.randomCustHour = function (){
-  var randomNum = Math.floor(Math.random() * 50);
-  console.log(randomNum);
-  return randomNum;
-};
-// SalmonCookieStores.prototype.randomCookieHour = function(){
-//   var randomCookieHour = Math.floor(Math.random() * (this.maxHourCust - this.minHourCust) + this.minHourCust);
-//   return randomCookieHour;
-//   // randomCookie.push(randomCookieHour);
-
-// };
-SalmonCookieStores.prototype.cookiePur = function(){
-  var result = Math.floor(Math.random() * (this.maxHourCust - this.minHourCust) + this.minHourCust);
-  // var randomCookieHour = Math.floor(Math.random() * (this.maxHourCust - this.minHourCust) + this.minHourCust);
-  return result;
-};
-// randomCookie.push(this.cookiePur());
-// sumRandomCookie += randomCookie;
-
-SalmonCookieStores.prototype.render = renderStores;
-
-//--------------------------
-// Define all data
-//--------------------------
-
-var allCookieStores = [];
-var hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-
-// console.log(randomCookie);
 //-----------------
 //Run Script
 //-----------------
 var cookieStoresEl = document.getElementById('cookieStores'); //Step 1
 console.log(cookieStoresEl);
 
-var storeOneFromConstructorFunction = new SalmonCookieStores('Cookie Store One', '1st and Pike', 23, 65, 6.3);
+// var storeOneFromConstructorFunction = new SalmonCookieStores('Cookie Store One', '1st and Pike', 23, 65, 6.3);
+// console.log(storeOneFromConstructorFunction);
+// storeOneFromConstructorFunction.cookieStoreTableHeadCell();
+storeOneFromConstructorFunction.render(cookieStoresEl);
+// storeTworomConstructorFunction.render(cookieStoresEl);
 
 
-for(var i = 0; i < allCookieStores.length; i++){
-  allCookieStores[i].render(cookieStoresEl);
-}
 
-console.log(storeOneFromConstructorFunction.randomCookie);
-// console.log(storeOneFromConstructorFunction.randomCookie);
-// console.log(allCookieStores);
-// for(var i = 0; i < allCookieStores.length; i++){
-//   randomCookie[i].render(cookieStoresEl);
+
+
+///----------------
+///EVENT HANDLERS
+///------------------
+
+// var addStoreEl = document.getElementById('addStore');
+// var addStoreEventListener = function(event){
+//   addStoreEl.addEventListener('submit', function(event){
+//     console.log(event);
+//     event.preventDefault();
+//     var target = event.target;
+//     var name = target.name.value;
+//     var location = target.name.value;
+//     var minCustomers = target.minCustomers.value;
+//     var maxCustomers = target.maxCustomers.value;
+  
+//     var newStore = new SalmonCookieStores(name, location, minCustomers, maxCustomers);
+//     newStore.render(document.getElementById('newStoreInfo'));
+//   });
 // }
+// addStoreEl.addEventListener('submit, add)
+
+// addStoreEl.addEventListener('submit', function(event){
+//   console.log(event);
+//   event.preventDefault();
+//   var target = event.target;
+//   var name = target.name.value;
+//   var location = target.name.value;
+//   var minCustomers = target.minCustomers.value;
+//   var maxCustomers = target.maxCustomers.value;
+
+//   var newStore = new SalmonCookieStores(name, location, minCustomers, maxCustomers);
+//   newStore.render(document.getElementById('newStoreInfo'));
+// });
 
 
